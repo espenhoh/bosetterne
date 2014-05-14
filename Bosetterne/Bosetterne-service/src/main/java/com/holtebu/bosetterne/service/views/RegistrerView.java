d02f43c8784ebde05004c86844bf087830d3b601
Espen Holtebu <espen.holtebu@gmail.com>
2014-05-14 22:24:20 +0200

    Fikset på registrering litt

--- Bosetterne/Bosetterne-service/src/main/java/com/holtebu/bosetterne/service/views/RegistrerView.java
+++ Bosetterne/Bosetterne-service/src/main/java/com/holtebu/bosetterne/service/views/RegistrerView.java
@@ -1,11 +1,18 @@
 package com.holtebu.bosetterne.service.views;
 
 
+import com.holtebu.bosetterne.api.Spiller;
+
 import io.dropwizard.views.View;
 
 public class RegistrerView extends View {
 
-	private boolean loggedIn = true;
+	private boolean loggedIn = false;
+	private String brukernavnEksisterer = "";
+	private String fargeEksisterer = "";
+	private String epostEksisterer = "";
+	
+	private Spiller registrertSpiller;
 
 	public RegistrerView(String templateName) {
 		super(templateName);
@@ -17,5 +24,53 @@ public class RegistrerView extends View {
 
 	public void setLoggedIn(boolean loggedIn) {
 		this.loggedIn = loggedIn;
+	}
+	
+	public boolean isRegistrert(){
+		return registrertSpiller != null;
+	}
+
+	public Spiller getRegistrertSpiller() {
+		return registrertSpiller;
+	}
+
+	public void setRegistrertSpiller(Spiller registrertSpiller) {
+		this.registrertSpiller = registrertSpiller;
+	}
+
+	public String getBrukernavnEksisterer() {
+		return brukernavnEksisterer;
+	}
+
+	public void setBrukernavnEksisterer(boolean brukernavnEksisterer) {
+		if (brukernavnEksisterer) {
+			this.brukernavnEksisterer = "<span id=\"brukernavn_fail\" class=\"feilmelding\" hidden><strong>Brukernavnet eksisterer, velg et annet.</strong></span>";
+		} else {
+			this.brukernavnEksisterer = "";
+		}
+	}
+
+	public String getFargeEksisterer() {
+		return fargeEksisterer;
+	}
+
+	public void setFargeEksisterer(boolean fargeEksisterer) {
+		if (fargeEksisterer) {
+			this.brukernavnEksisterer = "<span id=\"farge_fail\" class=\"feilmelding\" hidden><strong>Fargen eksisterer, velg en annen.</strong></span>";
+		} else {
+			this.brukernavnEksisterer = "";
+		}
+	}
+
+	public String getEpostEksisterer() {
+		return epostEksisterer;
+	}
+
+	public void setEpostEksisterer(boolean epostEksisterer) {
+		if (epostEksisterer) {
+			this.brukernavnEksisterer = "<span id=\"epost_fail\" class=\"feilmelding\" hidden><strong>Eposten eksisterer, du er fucked!</strong></span>";
+		} else {
+			this.brukernavnEksisterer = "";
+		}
 	}
 }
