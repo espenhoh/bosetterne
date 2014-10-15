fe1ec8e5a144bcc074289e9ba30d518de9aa722d
Espen Holtebu <espen.holtebu@gmail.com>
2014-10-15 23:15:33 +0200

    mysql 5.6

--- BosetterneModel/kodeverk/kdv-0.0.1.sql
+++ BosetterneModel/kodeverk/kdv-0.0.1.sql
@@ -12,7 +12,7 @@ INSERT INTO bosetterne.SPILLER (brukernavn, kallenavn, farge, epost, passord, da
 INSERT INTO bosetterne.SPILLER (brukernavn, kallenavn, farge, epost, passord, dato_registrert) 
 	VALUES ('testbruker2', 'testbruker', '#0000A0', 'test@test2.com', 'test_passord', CURDATE());
 
-insert into bosetterne.SPILL (leder, navn, type_spill, dato_fom, dato_tom)
-	values ('testbruker', 'testspill', 'BOSETTERNE', '2014-10-12 21:14:07', '2014-10-13 01:14:07');
-
-inster
+insert into bosetterne.SPILL (navn, leder, type_spill, dato_fom, dato_tom)
+	values ('testspill', 'testbruker', 'BOSETTERNE', '2014-10-12 21:14:07', '2014-10-13 01:14:07');
+insert into bosetterne.SPILL (navn, leder, type_spill, dato_fom, dato_tom)
+	values ('testspill', 'testbruker', 'BOSETTERNE', '2014-10-12 21:14:07', null);
